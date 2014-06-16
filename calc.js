089558463ab659d3a5e5a89849c325c01a562227
Michael Kreil <github@michael-kreil.de>
2014-06-16 20:27:13 +0200

    adapt file structure and generate "dna"

--- calc.js
+++ calc.js
@@ -1,11 +1,11 @@
 var Points = require('points');
 
-var points;
+var points, filename;
 //points = Points.getRandomPoints(1000, 100);
 
 
-/*Würfel*/points = Points.getCube(10, 150);points.rotateY(60);points.rotateX(30);points.shiftZ(-300);
-//*DNA*/ points = Points.getJSON('dna.json');points.center();points.rotateX(90);points.rotateY(120);points.scale(8);
+//filename = 'cube'; points = Points.getCube(10, 150);points.rotateY(60);points.rotateX(30);points.shiftZ(-300);
+//filename = 'dna'; points = Points.getJSON('data/dna.json');points.center();points.rotateX(90);points.rotateY(120);points.scale(8);
 
 
 
@@ -16,6 +16,6 @@ points.setCam({ x:0, y:    0, z:  500 });
 points.calcCurves();
 
 points.saveJS('./web/data.js');
-points.saveSVG('./cube.svg');
+points.saveSVG('./svg/'+filename+'.svg');
 
 
