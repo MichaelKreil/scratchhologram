fd5a993719d9f03dcbf4bef2e81d2afed5494b34
Michael Kreil <github@michael-kreil.de>
2014-06-17 02:43:53 +0200

    add random stars

--- calc.js
+++ calc.js
@@ -1,12 +1,12 @@
 var Points = require('points');
 
 var points, filename, blur;
-//points = Points.getRandomPoints(1000, 100);
 
 
 //filename = 'cube'; points = Points.getCube(10, 150);points.rotateY(60);points.rotateX(30);points.shiftZ(-300);
 //filename = 'dna'; points = Points.getJSON('data/dna.json');points.center();points.rotateX(90);points.rotateY(120);points.scale(10);
 //filename = 'skull'; blur = 2; points = Points.getFromCT('data/SE000006/');points.center();points.rotateY(180);points.shiftZ(-220);points.scale(1.5);
+filename = 'stars'; points = Points.getStars(1000);points.shiftZ(-1.5);points.scale(1e10);
 
 
 
