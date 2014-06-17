d5deb9c03923d6c9578325fa6273ed741686c113
Michael Kreil <github@michael-kreil.de>
2014-06-17 02:51:06 +0200

    refactor

--- calc.js
+++ calc.js
@@ -1,12 +1,13 @@
-var Points = require('points');
+var Points = require('points').Points;
+var generator = require('points').generator;
 
 var points, filename, blur;
 
 
-//filename = 'cube'; points = Points.getCube(10, 150);points.rotateY(60);points.rotateX(30);points.shiftZ(-300);
-//filename = 'dna'; points = Points.getJSON('data/dna.json');points.center();points.rotateX(90);points.rotateY(120);points.scale(10);
-//filename = 'skull'; blur = 2; points = Points.getFromCT('data/SE000006/');points.center();points.rotateY(180);points.shiftZ(-220);points.scale(1.5);
-filename = 'stars'; points = Points.getStars(1000);points.shiftZ(-1.5);points.scale(1e10);
+//filename = 'cube'; points = new Points(generator.getCube(10, 150)); points.rotateY(60);points.rotateX(30);points.shiftZ(-300);
+//filename = 'dna'; points = new Points(generator.getJSON('data/dna.json')); points.center();points.rotateX(90);points.rotateY(120);points.scale(10);
+//filename = 'skull'; blur = 2; points = new Points(generator.getFromCT('data/SE000006/')); points.center();points.rotateY(180);points.shiftZ(-220);points.scale(1.5);
+filename = 'stars'; points = new Points(generator.getStars(1000)); points.shiftZ(-1.5);points.scale(1e10);
 
 
 
